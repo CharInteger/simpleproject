a79a81dfc336315638112315a40ba1b888d53e13
CharInteger <afafafafafaf11@gmail.com>
2017-06-26 19:06:44 +0800

    Create Main.java

---
+++ simpleproject-core/src/main/java/pro/simpleproject/application/Main.java
@@ -0,0 +1,20 @@
+package
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
