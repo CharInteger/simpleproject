7a9d030c8460644bd09ac88111bf215d92c6636c
CharInteger <afafafafafaf11@gmail.com>
2017-07-31 18:38:17 +0800

    Update Simpleproject.java

--- simpleproject-core/src/main/java/pro/simpleproject/core/Simpleproject.java
+++ simpleproject-core/src/main/java/pro/simpleproject/core/Simpleproject.java
@@ -1,5 +1,11 @@
 package pro.simpleproject.core;
 
+import pro.simpleproject.core.start.Start;
+
 public class Simpleproject {
+
+    public static void main(String[] args) {
+        Start.launch(Start.class, args);
+    }
 
 }
