c1098baec70e1919ac838953879ff33fdeed685a
CharInteger <afafafafafaf11@gmail.com>
2017-08-07 18:32:23 +0800

    Update PrimaryPaneBuilder.java

--- simpleproject-core/src/main/java/pro/simpleproject/core/primary/PrimaryPaneBuilder.java
+++ simpleproject-core/src/main/java/pro/simpleproject/core/primary/PrimaryPaneBuilder.java
@@ -1,5 +1,29 @@
 package pro.simpleproject.core.primary;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
 public class PrimaryPaneBuilder {
+
+	private static BorderPane pane;
+
+	public static BorderPane getPane() {
+		return pane;
+	}
+
+	public Pane build() throws IOException {
+		pane = (BorderPane) FXMLLoader.load(getClass().getResource("primary.fxml"));
+		AnchorPane root = new AnchorPane(pane);
+		AnchorPane.setLeftAnchor(pane, 3.0);
+		AnchorPane.setRightAnchor(pane, 3.0);
+		AnchorPane.setTopAnchor(pane, 3.0);
+		AnchorPane.setBottomAnchor(pane, 3.0);
+		root.getStyleClass().add("base");
+		return root;
+	}
 
 }
