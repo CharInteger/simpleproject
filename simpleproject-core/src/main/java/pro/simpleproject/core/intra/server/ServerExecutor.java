39f9dd5e76686cf1edeeb3e0495f6dd4b48d3e4f
CharInteger <afafafafafaf11@gmail.com>
2017-11-10 18:47:52 +0800

    Create ServerExecutor.java

---
+++ simpleproject-core/src/main/java/pro/simpleproject/core/intra/server/ServerExecutor.java
@@ -0,0 +1,31 @@
+package pro.simpleproject.core.intra.server;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerExecutor {
+
+	private static ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	private static Server server;
+
+	public static void run(int port) {
+		if (server == null) {
+			executor.execute(() -> {
+				try {
+					server = new Server(port);
+					server.run();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+		}
+	}
+
+	public static void close() {
+		if (server != null) {
+			server.close();
+		}
+	}
+
+}
