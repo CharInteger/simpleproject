784dbc20e299bc5a49b4823d38d0bd4649f6e38c
CharInteger <afafafafafaf11@gmail.com>
2017-11-23 18:40:12 +0800

    Create EntryController.java

---
+++ simpleproject-core/src/main/java/pro/simpleproject/core/entry/EntryController.java
@@ -0,0 +1,26 @@
+package pro.simpleproject.core.entry;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
+import pro.simpleproject.core.extra.ExtraContactPaneBuilder;
+import pro.simpleproject.core.primary.PrimaryPaneBuilder;
+
+public class EntryController {
+
+	public Pane pane_base;
+	public ComboBox<String> combobox0;
+	public Button button0;
+
+	public void commit() {
+		String v = combobox0.getValue();
+		if (v != null && v.length() > 0) {
+			boolean e = EntryService.run(v);
+			if (e) {
+				PrimaryPaneBuilder.getPane().setLeft(ExtraContactPaneBuilder.get());
+				PrimaryPaneBuilder.getPane().setCenter(null);
+			}
+		}
+	}
+
+}
