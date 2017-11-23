0ca77063d28087d0a582a180ce5174c25acfa3d3
CharInteger <afafafafafaf11@gmail.com>
2017-11-23 18:33:32 +0800

    Update PrimaryPaneBuilder.java

--- simpleproject-core/src/main/java/pro/simpleproject/core/primary/PrimaryPaneBuilder.java
+++ simpleproject-core/src/main/java/pro/simpleproject/core/primary/PrimaryPaneBuilder.java
@@ -1,7 +1,5 @@
 package pro.simpleproject.core.primary;
 
-import java.io.IOException;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -15,16 +13,21 @@ public class PrimaryPaneBuilder {
 		return pane;
 	}
 
-	public static Pane build() throws IOException {
-		pane = (BorderPane) FXMLLoader.load(PrimaryPaneBuilder.class.getResource("primary.fxml"));
-		pane.getStylesheets().add(PrimaryPaneBuilder.class.getResource("primary.css").toExternalForm());
-		AnchorPane root = new AnchorPane(pane);
-		AnchorPane.setLeftAnchor(pane, 3.0);
-		AnchorPane.setRightAnchor(pane, 3.0);
-		AnchorPane.setTopAnchor(pane, 3.0);
-		AnchorPane.setBottomAnchor(pane, 3.0);
-		root.getStyleClass().add("base");
-		return root;
+	public static Pane build() {
+		try {
+			pane = (BorderPane) FXMLLoader.load(PrimaryPaneBuilder.class.getResource("primary.fxml"));
+			pane.getStylesheets().add(PrimaryPaneBuilder.class.getResource("primary.css").toExternalForm());
+			AnchorPane root = new AnchorPane(pane);
+			AnchorPane.setLeftAnchor(pane, 3.0);
+			AnchorPane.setRightAnchor(pane, 3.0);
+			AnchorPane.setTopAnchor(pane, 3.0);
+			AnchorPane.setBottomAnchor(pane, 3.0);
+			root.getStyleClass().add("base");
+			return root;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
