44e61f8c18aa2612a99e7a38fb58e7b53bf3c1a0
CharInteger <afafafafafaf11@gmail.com>
2018-09-25 19:12:31 +0800

    Update PrimaryService.java

--- simpleproject-core/src/main/java/pro/simpleproject/core/primary/PrimaryService.java
+++ simpleproject-core/src/main/java/pro/simpleproject/core/primary/PrimaryService.java
@@ -1,5 +1,19 @@
 package pro.simpleproject.core.primary;
 
+import javafx.application.Platform;
+import pro.simpleproject.core.intra.model.IntraModel;
+import pro.simpleproject.core.tray.TrayBuilder;
+
 public class PrimaryService {
+
+	public static boolean entry() {
+		return true;
+	}
+
+	public static void close() {
+		IntraModel.close();
+		Platform.exit();
+		TrayBuilder.close();
+	}
 
 }
