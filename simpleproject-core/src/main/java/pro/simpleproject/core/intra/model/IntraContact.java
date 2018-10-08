2cb257e66fef59b655499f2beb937ee101e874ac
CharInteger <afafafafafaf11@gmail.com>
2018-10-08 18:37:30 +0800

    Update IntraContact.java

--- simpleproject-core/src/main/java/pro/simpleproject/core/intra/model/IntraContact.java
+++ simpleproject-core/src/main/java/pro/simpleproject/core/intra/model/IntraContact.java
@@ -1,20 +1,13 @@
 package pro.simpleproject.core.intra.model;
 
-import pro.simpleproject.core.intra.client.Client;
-
 public class IntraContact {
 
 	private String address;
 	private int port;
-	private Client client;
 
 	public IntraContact(String address, int port) {
 		this.address = address;
 		this.port = port;
-	}
-
-	public IntraContact(String address) {
-		this(address, 0);
 	}
 
 	public String getAddress() {
@@ -29,20 +22,11 @@ public class IntraContact {
 		return port;
 	}
 
-	public Client getClient() {
-		if (client == null) {
-			if (port == 0) {
-				return null;
-			}
-			client = new Client(address, port);
-		}
-		return client;
+	public boolean write(Object o) {
+		return false;
 	}
 
 	public void close() {
-		if (client != null) {
-			client.close();
-		}
 	}
 
 }
